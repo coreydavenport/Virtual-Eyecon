d95c8b13f3762d5c7d6f429f1a50c7b1b7d3a7af
Corey Davenport <cairnelan@gmail.com>
2015-12-30 17:18:32 -0500

    Added Functionality
    - Added styling
    - Added click to change eyecon image functionality

--- js/app.js
+++ js/app.js
@@ -5,3 +5,47 @@
 	// 1.1 White overlay fadeout
 	// 1.2 Remove current Eyecon image
 	// 1.3 Display next Eyecon image
+		
+
+//Declare an image array same as calling new Array();
+    var img = []
+    img[0] = "http://pre12.deviantart.net/ffeb/th/pre/f/2015/232/a/0/kamen_rider_ghost_ore_eyecon_letter_by_raidenzein-d96iu1b.png";
+    img[1] = "http://pre10.deviantart.net/8de8/th/pre/f/2015/232/4/9/kamen_rider_ghost_ore_eyecon_form_by_raidenzein-d96iu1o.png";
+    img[2] = "http://pre08.deviantart.net/22d5/th/pre/f/2015/232/3/d/kamen_rider_ghost_ore_eyecon_attack_by_raidenzein-d96iu2f.png";
+    img[3] = "http://pre08.deviantart.net/d7df/th/pre/f/2015/232/3/0/kamen_rider_ghost_eyecon_by_raidenzein-d96iu0w.png";
+
+    //Select all elements on the page with the name attribute equal to eyecon
+    var images = document.querySelectorAll('[name=eyecon]');
+    var $overlay = $('<div id="overlay"></div>');
+    $("body").append($overlay);
+    //For each image bind the click event
+    for(var i=0; i < images.length; i++)
+    {
+      var image = images[i];
+      //https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener
+      image.addEventListener('click', imageClicked(), false);
+    }
+    $(".eyeconImg").click(function (){
+    	
+    	$overlay.show().fadeOut(2000);
+
+    });
+    
+    	
+    function imageClicked()
+    {
+
+      //Use a closure to wrap the counter variable
+      //so each image element has their own unique counter
+      var counter = 0;
+      return function(event)
+      {
+
+        //Increment counter
+        counter++;
+        //The context of "this" is the image element
+        //Use a modulus
+        this.src = img[counter % img.length];
+
+      }
+    }
