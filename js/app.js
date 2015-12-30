159ad4809bd6d69c7e3cbbc3709dcb249b7071d9
Corey Davenport <cairnelan@gmail.com>
2015-12-30 16:28:12 -0500

    Styling
    added styling

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//Problem: Make application that simulates Ghost Eyecon effect
+//Solution: Treat Eyecon images as buttons that hide when clicked, and are replaced by the next Eyecon image
+
+// 1. On click
+	// 1.1 White overlay fadeout
+	// 1.2 Remove current Eyecon image
+	// 1.3 Display next Eyecon image
